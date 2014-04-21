7f6e86dbdd360b29f37d3dbd2b08e0c0f6850450
Fredrik Sommar <fred.sommar+github@gmail.com>
2014-04-21 03:16:25 +0200

    Implemented some simple responses based on input.

--- server/src/com/dreamteam/test/vicam/CameraServer.java
+++ server/src/com/dreamteam/test/vicam/CameraServer.java
@@ -1,7 +1,52 @@
 package com.dreamteam.test.vicam;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static spark.Spark.get;
+
 /**
  * Created by fsommar on 2014-04-21.
  */
 public class CameraServer {
+
+
+        public static void main(String[] args) {
+            Map<String, String> responses = new HashMap<>();
+            responses.put("APC", "aPC45006300");
+            responses.put("GZ", "gz646");
+            responses.put("GF", "gfabc");
+
+            get(new Route("/cgi-bin/aw_ptz") {
+                @Override
+                public Object handle(Request request, Response response) {
+                    Optional<String> opt = Optional.of(request.queryMap("cmd").value());
+                    opt = opt.filter(s -> s.startsWith("#")).map(s -> s.substring(1));
+                    if (opt.isPresent()) {
+                        String res = opt.get();
+                        switch (res.length()) {
+                            case 2:
+                            case 3:
+                                return responses.getOrDefault(res, "error!");
+                            default:
+                                return res;
+                        }
+                    }
+                    return "error!";
+                }
+            });
+
+            get(new Route("/cgi-bin/aw_cam") {
+                @Override
+                public Object handle(Request request, Response response) {
+                    return "Hello cam!";
+                }
+            });
+
+        }
 }
